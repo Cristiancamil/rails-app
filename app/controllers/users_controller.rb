5979ee293085bdc658dac2c49cda35badfd0262f
Cristian Lopez <crca9623@gmail.com>
2023-08-21 02:56:23 +0000

    Agregar cambios de los metodos faltantes del modulo user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[new create]
+  before_action :set_user, only: %i[show edit update destroy]
+
+  def index
+    @user = User.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
 
   def new
+    @user = User.new
   end
 
   def create
@@ -11,13 +23,31 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path, notice: "Sign up sucessfully"
     else
-      render :new, status: :unproccessable_entity
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def update
+    if set_user.update(user_params)
+      redirect_to user_path(@user), notice: 'User was update'
+    else
+      render edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil if current_user == @user
+    @user.destroy
+    redirect_to users_path, notice: 'User was successfolly deleted.'
   end
 
   private
 
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
-    params.require(:session).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   delete  "/logout", to: "sessions#destroy", as: "logout"
 
   resources :articles
+  resources :users
 end
